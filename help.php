98bbc5ab9eddbe468309fa706f5ad8a8f2756130
letytesfu <59553567+letytesfu@users.noreply.github.com>
2020-01-21 15:56:52 +0300

    Add files via upload

---
+++ help.php
@@ -0,0 +1,42 @@
+
+<html>
+   <head>
+      <meta charset="utf-8">
+	  <title>Voter Login</title>
+   </head>
+   <body>
+      <h1>This page is for Help</h1>
+	  
+     <p><h4 style="text-align:center;"> INTRODUCTION</h4>
+			
+         Election is the right of making a choice or to vote freely for the candidate of one's choice. Election were used as early in history as 
+         ancient Greece and ancient Rome, but the modern one has operated since the 17th century.Since that people start to vote directly or indirectly 
+         through their representative.A lot of election was 1st hand based polling system then when the number of population was increased changed to 
+         paper based system.But most of the time paper based system electoral was not fair because many document was missed.
+         
+         Online election system was start in 21th century,But it is only in developed countries.Most of developing country still now use paper based 
+         electoral system.  
+         
+         Election system we are going to develop is for ASTU university,that can help with elect president of student,representative of the class----etc.This system is mainly used for disabled people also it is the best acess to vote in this system than paper based system.
+         
+         <h4 style=text-align:center;>BACKGROUND OF THE PROJECT</h4>
+         Online election system we are going to develop the web is the process of introducing fair electoral systems and fast,easy way of conducting election.
+         Voters will get Unique ID and Password from admin.Each voter vote only once,can vote if registered and there is also privacy to keep secret of voters.
+         </p>
+         <br>
+      <p><h4 style="text-align:center;">User of online election system</h4>
+      <h5>A. voters</h5> 
+ make a choice between options, often in an election or on a policy referendum 
+ encrypt in the voting servers by using special public voting keys
+<h5>B. admin</h5>
+Add voters-the administrator can add a new and unregistered voter or aspirant to the database in other to allow him or her vote during election
+update voters-update voters’ information. In case a voter changes his or her name, telephone number, location, program and level, the admin can change that information to reflect in the database.
+Delete voters- can delete a voter or multiple voters from the database by clicking on the delete button. This will help the administrator delete voters that are not supposed to vote
+view result-administrator track the voting process and also help him in the declaration of election results of the various positions. However, this can only be done when an election has closed
+Stop Election: the administrator can stop the voting process when time is due just by clicking 
+<h5>C. candidate.</h5>
+The software system allows the Candidate to login in to their profiles and upload all their details including their previous milestone onto the system
+      </p>
+      <a href="includes/logout.php" >Home Page</a>
+   </body>
+</html>
